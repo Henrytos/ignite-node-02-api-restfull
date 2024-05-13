f2eeebc97565e9bec90e9edc610ba744d296644a
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-05-13 15:55:34 -0300

    arrumando erros de deploy

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ const port: number = 3333
 app
   .listen({
     port,
+    host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('Server is running on port ' + port)
