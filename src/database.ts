0ef70967fed4f8dce9113f76cf876249f25d3ed3
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-05-13 15:26:48 -0300

    chore: prepare deploy

--- src/database.ts
+++ src/database.ts
@@ -2,11 +2,13 @@ import 'dotenv/config'
 import { knex as knexSetup, Knex } from 'knex'
 import { env } from './env'
 
+console.log(env.DATABASE_URL)
+
 export const config: Knex.Config = {
   client: 'sqlite3',
-  connection: {
+  connection: env.DATABASE_CLIENT === 'sqlite'?{
     filename: env.DATABASE_URL,
-  },
+  }:env.DATABASE_URL,
   migrations: {
     extension: 'ts',
     directory: './db/migrations',
