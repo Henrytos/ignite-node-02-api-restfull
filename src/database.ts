d48e28d91f0500ecca2c5475f5738384b86957de
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-05-13 15:27:20 -0300

    chore: prepare deploy  database

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import { env } from './env'
 console.log(env.DATABASE_URL)
 
 export const config: Knex.Config = {
-  client: 'sqlite3',
+  client: env.DATABASE_CLIENT,
   connection: env.DATABASE_CLIENT === 'sqlite'?{
     filename: env.DATABASE_URL,
   }:env.DATABASE_URL,
